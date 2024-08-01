ad2926e732d275fd984ee7b06d7c4e0a4ca55c08
jdkfx <amateurranch@gmail.com>
2024-08-01 20:56:07 +0900

    :sparkles: chapter19

---
+++ 19/unsafe_rust/src/main.rs
@@ -0,0 +1,46 @@
+use std::slice;
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+fn main() {
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1: {}", *r1);
+        println!("r2: {}", *r2);
+    }
+
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    unsafe {
+        println!("-3の絶対値は、Cによると{}です", abs(-3));
+    }
+}
+
+unsafe fn dangerous() {
+    // snip
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+    assert!(mid <= len);
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid)
+        )
+    }
+}
